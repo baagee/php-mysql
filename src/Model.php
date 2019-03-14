c72cc11e0d608cc775f902ca5fe321d53476eece
党留辉 <liuhuidang@sf-express.com>
2019-03-14 22:47:26 +0800

    init

---
+++ src/Model.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Desc:
+ * User: baagee
+ * Date: 2019/3/14
+ * Time: 下午10:45
+ */
+
+namespace BaAGee\MySQL;
+abstract class Model
+{
+
+}
---
+++ src/MySQL/DB.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Desc:
+ * User: baagee
+ * Date: 2019/3/14
+ * Time: 下午10:46
+ */
