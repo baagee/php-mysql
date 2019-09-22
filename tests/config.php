f49b88f82dbae07291ed86b89cabfce2e07c2bfa
党留辉 <liuhuidang@sf-express.com>
2019-09-22 21:39:14 +0800

    update

--- tests/config.php
+++ tests/config.php
@@ -5,35 +5,36 @@
  * Date: 2019/3/15
  * Time: 下午6:47
  */
+
 return [
-    'host'           => '',
-    'port'           => 3306,
-    'user'           => 'root',
-    'password'       => '',
-    'database'       => 'spiders_data',
-    'connectTimeout' => 1,
-    'charset'        => 'utf8mb4',
-    'retryTimes'     => 1,//重试次数
-    'options'        => [
+    'host'             => '127.0.0.1',
+    'port'             => 5200,
+    'user'             => 'aaa',
+    'password'         => '1q2w3e@sf',
+    'database'         => 'sss',
+    'connectTimeout'   => 1,
+    'charset'          => 'utf8mb4',
+    'retryTimes'       => 1,//重试次数
+    'schemasCachePath' => __DIR__ . '/schemas',
+    'options'          => [
         \PDO::ATTR_PERSISTENT => true,
-        \PDO::ATTR_CASE       => \PDO::CASE_LOWER,
     ],
-    'slave'          => [
+    'slave'            => [
         [
-            'host'           => '',
-            'port'           => 3306,
-            'user'           => 'root',
-            'password'       => '',
-            'database'       => 'spiders_data',
+            'host'             => '127.0.0.1',
+            'port'             => 5200,
+            'user'             => 'aaa',
+            'password'         => '1q2w3e@sf',
+            'database'         => 'sss',
             'connectTimeout' => 1,
             'weight'         => 3,//数据库从库权重，越大使用的频率越大
         ],
         [
-            'host'           => '',
-            'port'           => 3306,
-            'user'           => 'root',
-            'password'       => '',
-            'database'       => 'spiders_data',
+            'host'             => '127.0.0.1',
+            'port'             => 5200,
+            'user'             => 'aaa',
+            'password'         => '1q2w3e@sf',
+            'database'         => 'sss',
             'connectTimeout' => 1,
             'weight'         => 5,//数据库从库权重，越大使用的频率越大
         ]
